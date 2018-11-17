f5c210c63145f8dc3402e6dd6e553876261d797c
amir <amir@fermi.(none)>
2018-11-17 19:12:21 +0900

    cleanup

--- superscript.py
+++ superscript.py
@@ -1,24 +1,26 @@
 import pandas
-from ase.db import connect
 from abx3 import ABX3
 import sys
-import time
 import numpy
+import os
 
-csv_data = pandas.read_csv('ABX3cubDBnewbigRtol.csv')
+vlad1 = 'ABX3cubDBnewbigRtol.csv'
+vlad2 = 'ABX3cubDBGPAWRtol.csv'
+csv_data = pandas.read_csv( vlad2 )
+
 def get_csv(J):
     A, B, X = csv_data['A'][J], csv_data['B'][J], csv_data['X'][J]
     a = csv_data['RA'][J] + csv_data['RC'][J]
     return A, B, X, a
 
-try:
-    k0 = int(sys.argv[1])
-except IndexError:
-    k0=0
+def relaxations():
+    for k in range( csv_data.shape[0] ): 
+        A, B, X, a = get_csv(k)
+        abx3 = ABX3( A, B, X, a, process='creation')
+        if abx3.atoms is not None: 
+            print( "Found new job: {} indexed {} pid {}".format( A+B+X+'3', k, os.getpid() ) )
+        abx3.step1()
+        abx3.savestate('relaxed')
 
-for k in range( k0, csv_data.shape[0] ): 
-    A, B, X, a = get_csv(k)
-    abx3 = ABX3( A, B, X, a, process='creation')
-    time.sleep(0.01)
-    abx3.savestate('initial')
-
+if __name__=='__main__':
+    relaxations()
